927b87fa2207147beaa6690cc94b5203d25a90ea
justas-319 <justas@kurtinaitis.lt>
2026-05-19 22:50:30 +0300

    fixed problem by one in Game code checking ship lenght

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,6 +4,7 @@
 
 void Game::runSetup() {
     ai.setupGrid();
+    printBoard(ai.getMyGrid());
     static const int SHIP_SIZES[] = {5, 4, 3, 3, 2};
     static const int SHIP_COUNT = 5;
     for (int i = 0; i < SHIP_COUNT; ++i) {
@@ -11,6 +12,8 @@ void Game::runSetup() {
         bool placed = false;
         while (!placed) {
             int startX, startY, endX, endY;
+            int shipLenght = 0;
+            int horizontal = 0;
             std::cout << "Current ship's lenght: " << size << "\n";
             std::cout << "Please enter ship's start coordinates (e.g. 0 0): ";
             std::cin >> startX >> startY;
@@ -20,17 +23,23 @@ void Game::runSetup() {
                 std::cout << "No negative coordinates allowed.\n";
                 continue;
             }
-            if ((abs(startX-endX)>size)||(abs(startY-endY)>size)) {
+            shipLenght = abs(startX - endX) > abs(startY - endY) ? abs(startX - endX) : abs(startY - endY);
+            if (shipLenght > size-1) {
                 std::cout << "Ship too big.\n";
                 continue;
             }
+            if (shipLenght < size-1) {
+                std::cout << "Ship too small\n";
+                continue;
+            }
+            std::cout << endX;
             Ship ship(Coordinates(startX, startY), Coordinates(endX, endY));
             placed = human.placeShip(ship);
             if (!placed) {
-                std::cout << "can't place ship here.\n";
+                std::cout << "Can't place ship here.\n";
             }
         }
-        std::cout << "ship placed.\n";
+        std::cout << "Ship placed.\n";
         std::cout << "Current playfield:\n";
         printBoard(human.getMyGrid());
     }
